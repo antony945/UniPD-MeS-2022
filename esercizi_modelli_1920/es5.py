6d53c9000a0639e826028c420455f19f6efcaf64
antony945 <antonio.franzoso@outlook.it>
2022-01-13 17:02:37 +0100

    Added trial of es5 1920

---
+++ esercizi_modelli_1920/es5.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# encoding: utf-8
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+# SETS
+investimento = ["A", "B", "C", "D"]
+mesi = ["Aprile", "Maggio", "Giugno", "Luglio"]
+# PARAMETERS
+BUDGET = 100000
+OBIETTIVO = 150000
+durata_mesi = {"A": 1, "B": 2, "C": 3, "D": 1}
+rendimento = {"A": 0.1, "B": 0.19, "C": 0.33, "D": 0.15}
+rischio = {"A": 2, "B": 3, "C": 5, "D": 4}
+
+#################################################################################
+
+def buildModel():
+    model = ConcreteModel()
+    # SETS
+    model.investments = Set(initialize=investimento)
+    model.months = Set(initialize=mesi)
+    # PARAMETERS
+    model.duration = Param(model.investments, initialize=durata_mesi)
+    model.efficiency = Param(model.investments, initialize=rendimento)
+    model.risk = Param(model.investments, initialize=rischio)
+    model.BUDGET = Param(initialize=BUDGET)
+    model.GOAL = Param(initialize=OBIETTIVO)    
+    # VARIABLES
+    model.x = Var(model.investments, model.months, domain = NonNegativeIntegers)
+    model.invested = Var(model.months, domain = NonNegativeIntegers)
+    model.earned = Var(model.months, domain = NonNegativeReals)
+    model.capital = Var(model.months, domain = NonNegativeReals)
+    # variables constraints
+    model.invested_cst = Constraint(model.months, rule=lambda model,j:model.invested[j] == 1000*sum(model.x[i,j] for i in model.investments))
+    model.earned_cst_April = Constraint(expr = model.earned["Aprile"] == sum((1+model.efficiency[i])*1000*model.x[i,"Aprile"] for i in model.investments if model.duration[i]==1))
+    model.earned_cst_May = Constraint(expr = model.earned["Maggio"] == sum((1+model.efficiency[i])*1000*model.x[i,"Maggio"] for i in model.investments if model.duration[i]==1) +
+                                                sum((1+model.efficiency[i])*1000*model.x[i,"Aprile"] for i in model.investments if model.duration[i]==2))
+    model.earned_cst_June = Constraint(expr = model.earned["Giugno"] == sum((1+model.efficiency[i])*1000*model.x[i,"Giugno"] for i in model.investments if model.duration[i]==1) +
+                                                sum((1+model.efficiency[i])*1000*model.x[i,"Maggio"] for i in model.investments if model.duration[i]==2) +
+                                                sum((1+model.efficiency[i])*1000*model.x[i,"Aprile"] for i in model.investments if model.duration[i]==3))
+    model.earned_cst_July = Constraint(expr = model.earned["Luglio"] == sum((1+model.efficiency[i])*1000*model.x[i,"Luglio"] for i in model.investments if model.duration[i]==1) +
+                                                sum((1+model.efficiency[i])*1000*model.x[i,"Giugno"] for i in model.investments if model.duration[i]==2) +
+                                                sum((1+model.efficiency[i])*1000*model.x[i,"Maggio"] for i in model.investments if model.duration[i]==3))
+    model.capital_cst_April = Constraint(expr = model.capital["Aprile"] == model.BUDGET)
+    model.capital_cst_May = Constraint(expr = model.capital["Maggio"] == model.capital["Aprile"] - model.invested["Aprile"] + model.earned["Aprile"])
+    model.capital_cst_June = Constraint(expr = model.capital["Giugno"] == model.capital["Maggio"] - model.invested["Maggio"] + model.earned["Maggio"])
+    model.capital_cst_July = Constraint(expr = model.capital["Luglio"] == model.capital["Giugno"] - model.invested["Giugno"] + model.earned["Giugno"])
+    # model.capital_cst = Constraint(model.months, rule=lambda model,j:model.capital[j] == model.capital[j-1] - model.invested[j-1] + model.earned[j-1])
+    # OBJECTIVE FUNCTION
+    model.obj = Objective(expr=sum(model.risk[i]*sum(model.x[i,j] for j in model.months) for i in model.investments), sense=minimize)
+    # CONSTRAINTS
+    # budget monthly constraint
+    model.budget_cst = Constraint(model.months, rule=lambda model,j:model.invested[j] <= model.capital[j])
+    # reaching goal constraint
+    model.goal_cst = Constraint(expr = model.capital["Luglio"] - model.invested["Luglio"] + model.earned["Luglio"] >= model.GOAL)
+    return model
+
+if __name__ == '__main__':
+    model = buildModel()
+    opt = SolverFactory('glpk')
+    res = opt.solve(model)
+    model.display()
+    print("====================================================")
+    # Print variables
+    for p in model.x:
+        print("x[{}] = {}".format(p, value(model.x[p])))
